d5336f9e0057999a8c8fdda920e794012ec503ac
Ross-dch97 <dchross97@outlook.it>
2024-10-26 01:34:30 +0200

    Selfwork-operatori-2

---
+++ main.js
@@ -0,0 +1,13 @@
+let anno = 2024;
+let nascita = 1997;
+
+let età = anno - nascita ;
+
+console.log( "Hai tot anni:", età );
+
+let anni = 100 - età ;
+
+console.log("Ti mancano tot anni per arrivare a 100:", anni );
+
+
+
